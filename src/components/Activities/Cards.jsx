5ffe5f5eda6c77068d5a7d9a632212251931c291
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-10-24 23:44:46 -0300

    Feat: Activity duration
    activity duration varies accourding to the amount of hours

---
+++ src/components/Activities/Cards.jsx
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import soldOut from "../../assets/images/ant-design_close-circle-outlined.png";
+import available from "../../assets/images/pepicons_enter.png";
+
+export default function ActivityCard({ activity }) {
+    const calculateDuration = (startTime, endTime) => {
+    const startHour = parseInt(startTime.split(":")[0]);
+    const endHour = parseInt(endTime.split(":")[0]);
+    return endHour - startHour;
+  };
+
+  const durationHours = calculateDuration(
+    activity.hour.split(" - ")[0],
+    activity.hour.split(" - ")[1]
+  );
+
+ 
+  const heightPerHour = 79; 
+  const cardHeight = durationHours * heightPerHour;
+
+  return (
+    <CardContainer style={{ height: `${cardHeight}px` }}>
+      <ActivityLeft>
+        <Title>{activity.name}</Title>
+        <Hour>{activity.hour}</Hour>
+      </ActivityLeft>
+      <ActivityRight>
+        <Icon src={activity.soldOut ? soldOut : available} />
+        <Status activity={activity}>
+          {activity.soldOut ? "Esgotado" : `${activity.vagas} vagas`}
+        </Status>
+      </ActivityRight>
+    </CardContainer>
+  );
+}
+
+const CardContainer = styled.div`
+  display: flex;
+  width: 265px;
+  border-radius: 5px;
+  background-color: #F1F1F1;
+  border-top: 20px;
+  padding: 10px;
+`;
+const Title = styled.h1`
+    font-family: Roboto;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 14px;
+    letter-spacing: 0em;
+    text-align: left;
+    margin-bottom: 5px;
+`
+
+const Hour = styled.h1`
+    font-family: Roboto;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 14px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: #343434;
+`
+
+const ActivityLeft = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 75%;
+    height: 100%;
+`
+
+const ActivityRight = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 25%; 
+    border-left: 1px solid #CFCFCF;
+`
+const Icon = styled.img`
+    width: 20px;
+    height: 20px;
+`
+const Status = styled.h1`
+    font-family: Roboto;
+    font-size: 9px;
+    font-weight: 400;
+    line-height: 11px;
+    letter-spacing: 0em;
+    text-align: left;
+    margin-top: 5px;
+    color: ${props => (props.activity.soldOut ? "#CC6666" : "#247A6B")};
+`;
