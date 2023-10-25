f875277e8b04895d413ac7f6911d9784650aa18c
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-10-24 22:10:36 -0300

    Feat: Auditorium layout

--- src/components/Activities/index.jsx
+++ src/components/Activities/index.jsx
@@ -11,9 +11,31 @@ export default function ActivitiesReservation() {
                 <DateButton name = {'Sábado 23/10'}/>
                 <DateButton name = {'Domingo 24/10'}/>
             </Container>
+            <LabelContainer>
+                <Label>Auditorio principal</Label>
+                <Label>Auditorio lateral</Label>
+                <Label>Sala de workshop</Label>
+            </LabelContainer>
+            <AuditoriumContainer>
+                <Rooms></Rooms>
+                <Rooms></Rooms>
+                <Rooms></Rooms>
+            </AuditoriumContainer>
         </>
-    )
+    );
 }
+
+const AuditoriumContainer = styled.div`
+    display: flex;
+    height: 391px;
+    width: 864px;
+    border: 1px solid #D7D7D7;
+`
+const Rooms = styled.div`
+    border-right: 1px solid #D7D7D7;
+    width: 288px;
+    height: 100%;
+`
 
 const Container = styled.div`
     display: flex;
@@ -36,4 +58,21 @@ const Subtitle = styled.h2`
     text-align: left;
     color:#8E8E8E;
     margin-top: 50px;
+`
+const LabelContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 864px;
+    height: 40px;
+    margin-top: 40px;
+`
+const Label = styled.h3`
+    font-family: Roboto;
+    font-size: 17px;
+    font-weight: 400;
+    line-height: 20px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #7B7B7B;
 `
