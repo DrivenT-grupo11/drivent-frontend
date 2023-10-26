c350897fc84b9f3cf84517b97697b7ac05f5efc9
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-10-26 20:46:36 -0300

    Feat: activity  cards

--- src/components/Activities/DateButton.jsx
+++ src/components/Activities/DateButton.jsx
@@ -1,22 +1,21 @@
-import styled from "styled-components"
-
-export default function DateButton({name}){
-    return(
-        <>
-            <Button>{name}</Button>
-        </>
-    )   
-}
-
-
-const Button = styled.button`
+import styled from "styled-components";
+export default function DateButton({ name,  onClick }) {
+    return (
+      <Button onClick={onClick}>
+        {name}
+      </Button>
+    );
+  }
+  
+  const Button = styled.button`
     width: 131px;
     height: 37px;
     border-radius: 4px;
-    background-color:  #E0E0E0;
     border: none;
-    margin-right:20px;
+    margin-right: 20px;
     margin-top: 20px;
     box-shadow: 0px 2px 10px 0px #00000040;
     cursor: pointer;
-`
+    background-color: ${(props) => (props.clicked ? 'orange' : '#E0E0E0')};
+    color: ${(props) => (props.clicked ? 'white' : 'black')};
+  `;
